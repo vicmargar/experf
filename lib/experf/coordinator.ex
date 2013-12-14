729454a557508a52a8bd0f52ad2d0b5ed1e638fb
Victor Martinez <vicmargar@gmail.com>
2013-12-14 18:28:49 +0000

    Syntax and comments to fix coordinator sleep

--- lib/experf/coordinator.ex
+++ lib/experf/coordinator.ex
@@ -1,25 +1,35 @@
-defrecord CoordinatorStatus, inserted: 0, executed: 0, concurrency: 0, max_concurrency: 0, pids: [], start: 0, rps: 0, executed_this_second: 0, num_requests: 0, finished: 0
+defrecord CoordinatorStatus,
+          inserted: 0,
+          executed: 0,
+          concurrency: 0,
+          max_concurrency: 0,
+          pids: [],
+          start: 0,
+          rps: 0,
+          executed_this_second: 0,
+          num_requests: 0,
+          finished: 0
 
 defmodule Experf.Coordinator do
   def start_coordination(concurrency, rps, num_requests, caller) do
     start = :erlang.now()
     status = CoordinatorStatus.new concurrency: concurrency, pids: HashDict.new, start: start, rps: rps, num_requests: num_requests
-    :ok = coordinate(status)
+    coordinate(status)
     caller <- {:finished, num_requests}
   end
 
   @doc """
   num_requests have finished
   """
-  def coordinate(CoordinatorStatus[num_requests: num_requests, finished: num_requests]) do
-    :ok
-  end
+  # def coordinate(CoordinatorStatus[num_requests: num_requests, finished: num_requests]) do
+  #   :ok
+  # end
 
   @doc """
   Handles :run_permission and :finished messages when max_concurrency HAS been reached.
   run_permission will only store the pid, :finished will ask a process to run
   """
-  def coordinate(CoordinatorStatus[concurrency: max_concurrency, max_concurrency: max_concurrency]=status) do
+  def coordinate(status = CoordinatorStatus[concurrency: max_concurrency, max_concurrency: max_concurrency]) do
     inserted             = status.inserted
     executed             = status.executed
     finished             = status.finished
@@ -27,13 +37,18 @@ defmodule Experf.Coordinator do
     executed_this_second = status.executed_this_second
     rps                  = status.rps
     start                = status.start
+    num_requests         = status.num_requests
     receive do
       { pid, :run_permission } ->
         pids = HashDict.put(pids, inserted + 1, pid)
         coordinate(status.inserted(inserted + 1).pids(pid))
       { pid, :finished } ->
-        {start, executed_this_second} = run(executed + 1, pids, start, rps, executed_this_second)
-        coordinate(status.executed(executed + 1).finished(finished + 1).executed_this_second(executed_this_second).start(start))
+        finished = finished + 1
+        IO.puts "finished #{inspect finished}"
+        unless finished == num_requests do
+          {start, executed_this_second} = run(executed + 1, pids, start, rps, executed_this_second)
+          coordinate(status.executed(executed + 1).finished(finished).executed_this_second(executed_this_second).start(start))
+        end
     end
   end
 
@@ -41,7 +56,7 @@ defmodule Experf.Coordinator do
   Handles :run_permission and :finished messages when max_concurrency HAS NOT been reached
   yet, in both cases a new process is asked to run
   """
-  def coordinate(CoordinatorStatus[]=status) do
+  def coordinate(status = CoordinatorStatus[]) do
     concurrency          = status.concurrency
     inserted             = status.inserted
     executed             = status.executed
@@ -50,14 +65,19 @@ defmodule Experf.Coordinator do
     executed_this_second = status.executed_this_second
     rps                  = status.rps
     start                = status.start
+    num_requests         = status.num_requests
     receive do
       { pid, :run_permission } ->
         pids = HashDict.put(pids, inserted + 1, pid)
         {start, executed_this_second} = run(executed + 1, pids, start, rps, executed_this_second)
         coordinate(status.inserted(inserted + 1).executed(executed + 1).concurrency(concurrency + 1).pids(pids).executed_this_second(executed_this_second).start(start))
       { pid, :finished } ->
-        {start, executed_this_second} = run(executed + 1, pids, start, rps, executed_this_second)
-        coordinate(status.executed(executed + 1).concurrency(concurrency + 1).pids(pids).start(start).executed_this_second(executed_this_second).finished(finished + 1))
+        finished = finished + 1
+        IO.puts "finished #{inspect finished}"
+        unless finished == num_requests do
+          {start, executed_this_second} = run(executed + 1, pids, start, rps, executed_this_second)
+          coordinate(status.executed(executed + 1).concurrency(concurrency + 1).pids(pids).start(start).executed_this_second(executed_this_second).finished(finished))
+        end
     end
   end
 
@@ -70,6 +90,8 @@ defmodule Experf.Coordinator do
     remaining = 1000000 - diff
     sleep = max(0,remaining)
     IO.puts "sleeping for #{inspect sleep} us"
+    # we can't sleep the coordinator, we need to somehow tell the worker to run after sleep microseconds
+    # the number of executed this second has to be based on the number of finished processes.
     :timer.sleep(round(sleep / 1000))
     run(n, pids, :erlang.now(), rps, 0)
   end
