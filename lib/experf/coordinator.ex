d21f75b2692aaa1afc381032ea436eed6f226401
Victor Martinez <vicmargar@gmail.com>
2013-12-14 00:51:15 +0000

    Reimplement using record

--- lib/experf/coordinator.ex
+++ lib/experf/coordinator.ex
@@ -1,14 +1,17 @@
+defrecord CoordinatorStatus, inserted: 0, executed: 0, concurrency: 0, max_concurrency: 0, pids: [], start: 0, rps: 0, executed_this_second: 0, num_requests: 0, finished: 0
+
 defmodule Experf.Coordinator do
   def start_coordination(concurrency, rps, num_requests, caller) do
     start = :erlang.now()
-    :ok = coordinate(0, 0, concurrency, 0, HashDict.new, start, rps, 0, num_requests, 0)
+    status = CoordinatorStatus.new concurrency: concurrency, pids: HashDict.new, start: start, rps: rps, num_requests: num_requests
+    :ok = coordinate(status)
     caller <- {:finished, num_requests}
   end
 
   @doc """
   num_requests have finished
   """
-  def coordinate(inserted, executed, max_concurrency, concurrency, pids, start, rps, executed_this_second, num_requests, num_requests) do
+  def coordinate(CoordinatorStatus[num_requests: num_requests, finished: num_requests]) do
     :ok
   end
 
@@ -16,14 +19,21 @@ defmodule Experf.Coordinator do
   Handles :run_permission and :finished messages when max_concurrency HAS been reached.
   run_permission will only store the pid, :finished will ask a process to run
   """
-  def coordinate(inserted, executed, max_concurrency, max_concurrency, pids, start, rps, executed_this_second, num_requests, finished) do
+  def coordinate(CoordinatorStatus[concurrency: max_concurrency, max_concurrency: max_concurrency]=status) do
+    inserted             = status.inserted
+    executed             = status.executed
+    finished             = status.finished
+    pids                 = status.pids
+    executed_this_second = status.executed_this_second
+    rps                  = status.rps
+    start                = status.start
     receive do
       { pid, :run_permission } ->
         pids = HashDict.put(pids, inserted + 1, pid)
-        coordinate(inserted + 1, executed, max_concurrency, max_concurrency, pids, start, rps, executed_this_second, num_requests, finished)
-      { pid, :finished, x } ->
+        coordinate(status.inserted(inserted + 1).pids(pid))
+      { pid, :finished } ->
         {start, executed_this_second} = run(executed + 1, pids, start, rps, executed_this_second)
-        coordinate(inserted, executed + 1, max_concurrency, max_concurrency, pids, start, rps, executed_this_second, num_requests, finished + 1)
+        coordinate(status.executed(executed + 1).finished(finished + 1).executed_this_second(executed_this_second))
     end
   end
 
@@ -31,15 +41,23 @@ defmodule Experf.Coordinator do
   Handles :run_permission and :finished messages when max_concurrency HAS NOT been reached
   yet, in both cases a new process is asked to run
   """
-  def coordinate(inserted, executed, max_concurrency, concurrency, pids, start, rps, executed_this_second, num_requests, finished) do
+  def coordinate(CoordinatorStatus[]=status) do
+    concurrency          = status.concurrency
+    inserted             = status.inserted
+    executed             = status.executed
+    finished             = status.finished
+    pids                 = status.pids
+    executed_this_second = status.executed_this_second
+    rps                  = status.rps
+    start                = status.start
     receive do
       { pid, :run_permission } ->
         pids = HashDict.put(pids, inserted + 1, pid)
         {start, executed_this_second} = run(executed + 1, pids, start, rps, executed_this_second)
-        coordinate(inserted + 1, executed + 1, max_concurrency, concurrency + 1, pids, start, rps, executed_this_second, num_requests, finished)
-      { pid, :finished, x } ->
+        coordinate(status.inserted(inserted + 1).executed(executed + 1).concurrency(concurrency + 1).pids(pids).executed_this_second(executed_this_second))
+      { pid, :finished } ->
         {start, executed_this_second} = run(executed + 1, pids, start, rps, executed_this_second)
-        coordinate(inserted, executed + 1, max_concurrency, concurrency + 1, pids, start, rps, executed_this_second, num_requests, finished + 1)
+        coordinate(status.executed(executed + 1).concurrency(concurrency + 1).pids(pids).start(start).executed_this_second(executed_this_second).finished(finished + 1))
     end
   end
 
