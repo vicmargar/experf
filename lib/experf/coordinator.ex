b72f81326991052d2e9cc95e9f112e9629cf9df0
Victor Martinez <vicmargar@gmail.com>
2013-12-15 01:31:05 +0000

    Fix warnings

--- lib/experf/coordinator.ex
+++ lib/experf/coordinator.ex
@@ -1,23 +1,23 @@
 defrecord CoordinatorStatus,
-          inserted: 0, # asked for permission to run
-          executed: 0, # started running
-          finished: 0, # finished running
+          inserted: 0, # num of processes that asked for permission to run
+          executed: 0, # num of processes that started running
+          executed_this_second: 0, # num of processes told to run in the current second
+          finished: 0, # num of processes that finished running
           concurrency: 0,
           max_concurrency: 0,
           pids: [],
           rps: 0,
-          executed_this_second: 0,
           num_requests: 0
 
 defmodule Experf.Coordinator do
   def start_coordination(concurrency, rps, num_requests, caller) do
-    timer = :erlang.send_after(1000, self, {:second})
+    :erlang.send_after(1000, self, {:second})
     status = CoordinatorStatus.new concurrency: concurrency, pids: HashDict.new, rps: rps, num_requests: num_requests
     :ok = coordinate(status)
     caller <- {:finished, num_requests}
   end
 
-  def coordinate(status = CoordinatorStatus[finished: num_requests, num_requests: num_requests]) do
+  def coordinate(CoordinatorStatus[finished: num_requests, num_requests: num_requests]) do
     :ok
   end
 
@@ -44,17 +44,18 @@ defmodule Experf.Coordinator do
         new_second(status)
       { pid, :run_permission } ->
         run_permission(pid, status)
-      { pid, :finished } ->
+      { _pid, _n, :finished } ->
         finished(status)
     end
   end
 
   defp new_second(status) do
-    timer = :erlang.send_after(1000, self, {:second})
+    IO.puts "#{inspect status.finished}/#{status.num_requests} requests finished"
+    :erlang.send_after(1000, self, {:second})
     coordinate(status.executed_this_second(0))
   end
 
-  def run_permission(pid, status = CoordinatorStatus[inserted: inserted, pids: pids]) do
+  defp run_permission(pid, status = CoordinatorStatus[inserted: inserted, pids: pids]) do
     pids = HashDict.put(pids, inserted + 1, pid)
     coordinate(status.inserted(inserted + 1).pids(pids))
   end
@@ -63,7 +64,7 @@ defmodule Experf.Coordinator do
     coordinate(status.finished(finished + 1).concurrency(concurrency - 1))
   end
 
-  defp run(n, status = CoordinatorStatus[pids: pids]) do
+  defp run(n, CoordinatorStatus[pids: pids]) do
     pid = HashDict.get(pids, n)
     if pid do
       pid <- {:run, n}
