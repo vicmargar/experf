3f0752e38014bec7e79586d15aa86ca40c8edba0
Victor Martinez <vicmargar@gmail.com>
2013-12-15 01:30:50 +0000

    cleanup code

--- lib/experf.ex
+++ lib/experf.ex
@@ -6,36 +6,20 @@ defmodule Experf do
   end
 
   def main(args) do
-    options = args |> parse_args |> process(HashDict.new)
-    print options
+    options = args |> parse_args
+    IO.puts "#{inspect options}"
 
-    num_requests = HashDict.get options, :num_requests
-    concurrency  = HashDict.get options, :concurrency
-    rps          = HashDict.get options, :rps
-    url          = HashDict.get options, :url
-    verbose      = HashDict.get options, :verbose
+    num_requests = options[:num_requests]
+    concurrency  = options[:concurrency]
+    rps          = options[:rps]
+    url          = options[:url]
+    verbose      = options[:verbose]
 
     {:ok, list_url} = String.to_char_list(url)
 
     start  = :erlang.now()
-
-    coordinator = spawn Experf.Coordinator, :start_coordination, [concurrency, rps, num_requests, self()]
-
-    job = fn(n) ->
-      {{_,_,_}, {h,m,s}} = :erlang.localtime()
-      {:ok, {{200, 'OK'}, headers, body}} = :lhttpc.request(list_url, 'GET', [], 5000)
-      if verbose do
-        IO.puts "Headers: #{inspect headers}"
-        IO.puts "Body: #{inspect body}"
-      else
-        IO.puts "returned 200 #{inspect n} #{inspect h}:#{inspect m}:#{inspect s}"
-      end
-    end
-
-    fun = fn(_) ->
-      spawn Experf.Worker, :run, [coordinator, job]
-    end
-    Enum.map(1..num_requests, fun)
+    coordinator = spawn_coordinator(concurrency, rps, num_requests)
+    spawn_workers(num_requests, list_url, verbose, coordinator)
 
     receive do
       {:finished, total} ->
@@ -45,27 +29,34 @@ defmodule Experf do
         IO.puts "#{inspect total} requests finished in #{diff / 1000000} secs"
         results = :gen_server.call(:experf, :results)
         mean = DescriptiveStatistics.mean(results)
-        IO.puts "Mean #{inspect mean / 1000} (ms)"
+        IO.puts "Mean #{inspect round(mean / 1000)} (ms)"
     end
   end
 
-  def parse_args(args) do
+  defp parse_args(args) do
     {options, _, _} = OptionParser.parse(args,
       switches: [num_requests: :integer, rps: :integer, concurrency: :integer, url: :string, verbose: :boolean],
       aliases:  [n: :num_requests, s: :rps, c: :concurrency, u: :url, v: :verbose]
     )
-
     options
   end
 
-  def process([], dict), do: dict
-
-  def process([{key, value} | tail], dict) do
-    dict = HashDict.put(dict, key, value)
-    process(tail, dict)
+  defp spawn_coordinator(concurrency, rps, num_requests) do
+    spawn Experf.Coordinator, :start_coordination, [concurrency, rps, num_requests, self()]
   end
 
-  def print(result) do
-    IO.puts "#{inspect result}"
+  defp spawn_workers(num_requests, url, verbose, coordinator) do
+    job = fn(n) ->
+      {{_,_,_}, {h,m,s}} = :erlang.localtime()
+      {:ok, {{200, 'OK'}, _headers, _body}} = :lhttpc.request(url, 'GET', [], 5000)
+      if verbose do
+        IO.puts "returned 200 #{inspect n} #{inspect h}:#{inspect m}:#{inspect s}"
+      end
+    end
+
+    fun = fn(_) ->
+      spawn Experf.Worker, :run, [coordinator, job]
+    end
+    Enum.each(1..num_requests, fun)
   end
 end
