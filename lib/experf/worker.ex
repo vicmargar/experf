0ae7a872a7ace8819d9f6032d20d296d2a2816e6
Victor Martinez <vicmargar@gmail.com>
2014-05-17 22:56:35 +0100

    Define worker functions as macros

--- lib/experf/worker.ex
+++ lib/experf/worker.ex
@@ -1,19 +1,27 @@
 defmodule Experf.Worker do
-  def run(coordinator, job) do
-    send(coordinator, {self(), :run_permission})
+  defmacro __using__(_) do
+    quote do
+      def a do
+        IO.puts "a!!!!"
+      end
 
-    receive do
-      {:run, n} ->
-        execute(n, job)
-        send(coordinator, {self(), n, :finished})
+      def run(coordinator, job) do
+        send(coordinator, {self(), :run_permission})
+
+        receive do
+          {:run, n} ->
+            execute(n, job)
+            send(coordinator, {self(), n, :finished})
+        end
+      end
+
+      def execute(n, job) do
+        start  = :erlang.now()
+        job.(n)
+        finish = :erlang.now()
+        diff = :timer.now_diff(finish, start)
+        :gen_server.cast(:results, {:push, diff})
+      end
     end
-  end
-
-  def execute(n, job) do
-    start  = :erlang.now()
-    job.(n)
-    finish = :erlang.now()
-    diff = :timer.now_diff(finish, start)
-    :gen_server.cast(:results, {:push, diff})
   end
 end
