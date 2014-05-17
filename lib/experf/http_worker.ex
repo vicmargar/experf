fdae5ed0f5250e7204308ec57f5e84b17e7b2986
Victor Martinez <vicmargar@gmail.com>
2014-05-17 23:33:51 +0100

    Implement job in Experf.HttpWorker

--- lib/experf/http_worker.ex
+++ lib/experf/http_worker.ex
@@ -1,5 +1,24 @@
 defmodule Experf.HttpWorker do
   use Experf.Worker
 
+  def job(n, options) do
+    {{_,_,_}, {h,m,s}} = :erlang.localtime()
 
+    url     = options[:url]
+    verbose = options[:verbose]
+
+    try do
+      %HTTPotion.Response{status_code: 200, body: body} = HTTPotion.get url
+
+      if verbose do
+        IO.puts "#{inspect body}"
+        IO.puts "returned 200 #{inspect n} #{inspect h}:#{inspect m}:#{inspect s}"
+      end
+      :ok
+    rescue
+      HTTPotion.HTTPError ->
+        IO.puts "Error!"
+        :error
+    end
+  end
 end
