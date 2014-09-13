a4df8db7fdc65c8d1135400f00ddf836ea263b5c
Victor Martinez <vicmargar@gmail.com>
2014-09-13 11:28:52 +0100

    parse command line options

--- lib/experf.ex
+++ lib/experf.ex
@@ -1,7 +1,16 @@
 require Logger
 
 defmodule Experf do
-  def main(_args) do
-    Logger.info "Hello World!"
+  def main(args) do
+    options = parse_args(args)
+
+    Logger.info inspect(options)
+  end
+
+  def parse_args(args) do
+    {options, _, _} = OptionParser.parse(args,
+      switches: [n: :integer, url: :string]
+    )
+    Enum.into(options, %{})
   end
 end
