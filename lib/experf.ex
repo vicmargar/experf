581440fd342c8af74aa9eab0a3026c66106c053f
Victor Martinez <vicmargar@gmail.com>
2015-06-25 00:24:28 +0100

    accept file option

--- lib/experf.ex
+++ lib/experf.ex
@@ -8,18 +8,18 @@ defmodule Experf do
   end
 
   def main(args) do
-    options = args |> parse_args |> Enum.into(%{})
+    options = args |> parse_args |> load_file |> Enum.into(%{})
     Logger.info "#{inspect options}"
 
-    start  = :erlang.now()
-
     coordinator_task = Task.async(Experf.Coordinator, :start_coordination, [options])
-
     spawn_workers(options, Experf.Coordinator)
 
+    start  = :erlang.now()
+    send(Experf.Coordinator, :start)
     Task.await(coordinator_task, :infinity)
-
     finish = :erlang.now()
+
+
     diff   = :timer.now_diff(finish, start)
 
     GenServer.call(Experf.Results, :results) |> print_results(diff)
@@ -27,19 +27,38 @@ defmodule Experf do
 
   defp parse_args(args) do
     {options, _, _} = OptionParser.parse(args,
-      switches: [num_requests: :integer, rps: :integer, concurrency: :integer, url: :string, verbose: :boolean],
-      aliases:  [n: :num_requests, s: :rps, c: :concurrency, u: :url, v: :verbose]
+      switches: [num_requests: :integer, rps: :integer, concurrency: :integer, url: :string, verbose: :boolean, file: :string],
+      aliases:  [n: :num_requests, s: :rps, c: :concurrency, u: :url, v: :verbose, f: :file]
     )
     options
   end
 
+  defp load_file(options) do
+    file = options[:file]
+    if file do
+      options = options ++ [urls: File.read!(file) |> String.split]
+    end
+    options
+  end
+
+  defp sample([]) do
+    nil
+  end
+  defp sample(list) when is_list(list) do
+    :lists.nth(:random.uniform(length(list)), list)
+  end
+
   defp spawn_workers(options, coordinator) do
     for _ <- 1..options[:num_requests] do
-      spawn Experf.HttpWorker, :run, [coordinator, options]
+      url_options = Dict.put(options, :url, sample(options[:urls]))
+
+      spawn Experf.HttpWorker, :run, [coordinator, url_options]
     end
   end
 
   defp print_results(%{success: success, errors: errors}, diff) do
+    Logger.info inspect(success)
+
     successful = length(success)
     mean       = DescriptiveStatistics.mean(success)
     stdev      = DescriptiveStatistics.standard_deviation(success)
